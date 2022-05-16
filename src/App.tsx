f71adeb8770afcfd2a97f13019274066a49abac2
origami-z <davidcui2@me.com>
2022-05-16 18:36:50 +0100

    Fix App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
+import * as React from "react";
+import { Button } from "@jpmorganchase/uitk-core";
+import { AddIcon, RemoveIcon } from "@jpmorganchase/uitk-icons";
 import { Card, Panel } from "@jpmorganchase/uitk-lab";
-import { Nav } from "./components/Nav";
 
-const App = () => (
-  <div>
-    <Panel>
-      <Card>
-        <h1>Welcome to your new app!</h1>
-      </Card>
-    </Panel>
-  </div>
-);
+const App = () => {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <Panel>
+        <Card>
+          <h1>Welcome to your new app!</h1>
+        </Card>
+      </Panel>
+      <Panel>
+        <h1>
+          <Button onClick={() => setCount((x) => x + 1)}>
+            <AddIcon />
+          </Button>{" "}
+          Counter {count}{" "}
+          <Button onClick={() => setCount((x) => x - 1)}>
+            <RemoveIcon />
+          </Button>
+        </h1>
+      </Panel>
+    </div>
+  );
+};
 
 export default App;
