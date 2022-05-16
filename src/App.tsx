6e4a6bd4413a0cc39564bc6462b5342d6b837f73
origami-z <davidcui2@me.com>
2022-05-16 16:52:55 +0100

    Basic set up

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Card, Panel } from "@jpmorganchase/uitk-lab";
+import { Nav } from "./components/Nav";
+
+const App = () => (
+  <div>
+    <Panel>
+      <Card>
+        <h1>Welcome to your new app!</h1>
+      </Card>
+    </Panel>
+  </div>
+);
+
+export default App;
