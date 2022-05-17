b946cfd8ac81b293373a8869902211eada9e4cad
origami-z <davidcui2@me.com>
2022-05-17 19:01:07 +0100

    New template content with theme toggle

--- src/__tests__/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "../App";
-
-test("renders welcome text", () => {
-  render(<App />);
-  const welcomeElement = screen.getByText(/Welcome to your new app/i);
-  expect(welcomeElement).toBeInTheDocument();
-});
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,12 @@
-import { ToolkitProvider } from "@jpmorganchase/uitk-core";
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-// Import UITK CSS before any component is loaded to get correct CSS order
-import "@jpmorganchase/uitk-theme/index.css";
-
-import App from "./App";
-import "./index.css";
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ToolkitProvider>
-      <App />
-    </ToolkitProvider>
+    <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
